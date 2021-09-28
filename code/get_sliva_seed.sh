690cee0c9fc9714c4eabe078f14b4136641a127f
Audrey <audrey.law@uky.edu>
2021-09-28 14:33:39 -0400

    line endings, LF in atom, changed back to clrf by git?

--- code/get_sliva_seed.sh
+++ code/get_sliva_seed.sh
@@ -5,6 +5,7 @@
 # outputs: places silva seed alignment into data/references/silva_seed
 # for aligning sequence data, version 138. Created a directory within data/references
 # because contained a README file
+# lf?
 
 wget -nc -P data/references/ https://mothur.s3.us-east-2.amazonaws.com/wiki/silva.seed_v138.tgz
 mkdir data/references/silva_seed
