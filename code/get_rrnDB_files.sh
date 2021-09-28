819619043973640cf1d34f97e327c5741d317a58
Audrey <audrey.law@uky.edu>
2021-09-28 12:12:19 -0400

    automate downloading sliva and rrnDB file, closes #8

---
+++ code/get_rrnDB_files.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# author: Audrey Law
+# input: name of file extracted from archive (without path)
+# output: rrnDB  files into data/raw
+
+archive=$1
+
+
+
+wget -P data/raw/ -nc https://rrndb.umms.med.umich.edu/static/download/"$archive".zip
+unzip -n -d data/raw/ data/raw/"$archive".zip
---
+++ code/get_sliva_seed.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# author: Audrey Law
+# inputs: none
+# outputs: places silva seed alignment into data/references/silva_seed
+# for aligning sequence data, version 138. Created a directory within data/references
+# because contained a README file
+
+wget -nc -P data/references/ https://mothur.s3.us-east-2.amazonaws.com/wiki/silva.seed_v138.tgz
+mkdir data/references/silva_seed
+tar xvzf data/references/silva.seed_v138.tgz -C data/references/silva_seed/
